b1c8399d94c4c176b60c63e13c07fe77cb21f40b
何小贤 <639859836@qq.com>
2016-10-19 08:55:37 +0800

    请求二级页面的数据加载

--- app/src/main/java/cn/ucai/fulicenter/net/NetDao.java
+++ app/src/main/java/cn/ucai/fulicenter/net/NetDao.java
@@ -3,6 +3,7 @@ package cn.ucai.fulicenter.net;
 import android.content.Context;
 
 import cn.ucai.fulicenter.I;
+import cn.ucai.fulicenter.bean.GoodsDetailsBean;
 import cn.ucai.fulicenter.bean.NewGoodsBean;
 
 /**
@@ -20,6 +21,14 @@ public class NetDao {
                 .addParam(I.PAGE_ID,String.valueOf(pageId))
                 .addParam(I.PAGE_SIZE,String.valueOf(I.PAGE_SIZE_DEFAULT))
                 .targetClass(NewGoodsBean[].class)
+                .execute(listener);
+    }
+    public static void downloadGoodDetails(Context context, int goodsId,
+                                           OkHttpUtils.OnCompleteListener<GoodsDetailsBean>listener){
+        OkHttpUtils<GoodsDetailsBean> utils=new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_FIND_GOOD_DETAILS)
+                .addParam(I.GoodsDetails.KEY_GOODS_ID,String.valueOf(goodsId))
+                .targetClass(GoodsDetailsBean.class)
                 .execute(listener);
     }
 }
