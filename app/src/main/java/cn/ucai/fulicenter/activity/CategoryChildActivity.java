7e72ff032b04d7adba53df80adb895cebe85fcdc
何小贤 <639859836@qq.com>
2016-10-21 11:39:28 +0800

    创建登录和注册的Activity且布局

--- app/src/main/java/cn/ucai/fulicenter/activity/CategoryChildActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/CategoryChildActivity.java
@@ -1,5 +1,6 @@
 package cn.ucai.fulicenter.activity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.GridLayoutManager;
@@ -165,13 +166,17 @@ public class CategoryChildActivity extends BaseActivity {
 
     @OnClick({R.id.btn_sort_price, R.id.btn_sort_addtime})
     public void onClick(View view) {
+        Drawable right;
         switch (view.getId()) {
             case R.id.btn_sort_price:
                 if(priceAsc){
                     sortBy=I.SORT_BY_PRICE_ASC;
+                    right=getResources().getDrawable(R.mipmap.arrow_order_up);
                 }else {
                     sortBy=I.SORT_BY_PRICE_DESC;
+                    right=getResources().getDrawable(R.mipmap.arrow_order_down);
                 }
+
                 priceAsc=!priceAsc;
                 break;
             case R.id.btn_sort_addtime:
