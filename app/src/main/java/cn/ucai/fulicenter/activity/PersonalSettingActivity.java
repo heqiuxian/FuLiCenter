5a7f59a12aeb4016ad9abed123be349fdbf43432
何小贤 <639859836@qq.com>
2016-10-26 00:15:32 +0800

    用户头像的更改 .(已知闪退bug)

--- app/src/main/java/cn/ucai/fulicenter/activity/PersonalSettingActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/PersonalSettingActivity.java
@@ -1,21 +1,30 @@
 package cn.ucai.fulicenter.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.ucai.fulicenter.FuLiCenterApplication;
+import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.bean.Result;
 import cn.ucai.fulicenter.bean.User;
 import cn.ucai.fulicenter.dao.SharePrefrenceUtils;
+import cn.ucai.fulicenter.net.NetDao;
+import cn.ucai.fulicenter.net.OkHttpUtils;
 import cn.ucai.fulicenter.utils.ImageLoader;
 import cn.ucai.fulicenter.utils.L;
 import cn.ucai.fulicenter.utils.MFGT;
+import cn.ucai.fulicenter.utils.OnSetAvatarListener;
+import cn.ucai.fulicenter.utils.ResultUtils;
 import cn.ucai.fulicenter.view.DisplayUtils;
 
 public class PersonalSettingActivity extends BaseActivity {
@@ -28,6 +37,7 @@ public class PersonalSettingActivity extends BaseActivity {
     TextView tvUserNick;
     PersonalSettingActivity mContext;
     User user=null;
+    OnSetAvatarListener mOnSetAvatarListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,16 +54,12 @@ public class PersonalSettingActivity extends BaseActivity {
 
     @Override
     protected void initData() {
-        user= FuLiCenterApplication.getUser();
+        user = FuLiCenterApplication.getUser();
         if(user==null){
-            MFGT.gotoLogin(mContext);
-            L.e("为空>>>>>>>>>>>进入登录"+user);
-        }else{
-            ImageLoader.setAvatar(ImageLoader.getAvatarUrl(user),mContext,ivUserAvatar);
-            tvUserName.setText(user.getMuserName());
-            tvUserNick.setText(user.getMuserNick());
-            L.e("不为空,设置个人资料参数>>>>>>>>>>");
+            finish();
+            return;
         }
+        showInfo();
     }
 
     @Override
@@ -64,17 +70,65 @@ public class PersonalSettingActivity extends BaseActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        user=FuLiCenterApplication.getUser();
-        if(user!=null){
-            ImageLoader.setAvatar(ImageLoader.getAvatarUrl(user),mContext,ivUserAvatar);
-            tvUserName.setText(user.getMuserName());
+        showInfo();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        L.e("onActivityResult,requestCode="+requestCode+",resultCode="+resultCode);
+        if(resultCode!=RESULT_OK){
+            return;
         }
+        mOnSetAvatarListener.setAvatar(requestCode,data,ivUserAvatar);
+        if(requestCode== I.REQUEST_CODE_NICK){
+            Toast.makeText(mContext, "修改昵称成功", Toast.LENGTH_SHORT).show();
+        }
+        if(requestCode==OnSetAvatarListener.REQUEST_CROP_PHOTO){
+            updateAvatar();
+        }
+    }
+
+    private void updateAvatar() {
+        File file = new File(OnSetAvatarListener.getAvatarPath(mContext,
+                user.getMavatarPath()+"/"+user.getMuserName()
+                        +I.AVATAR_SUFFIX_JPG));
+        
+        NetDao.updateAvatar(mContext, user.getMuserName(), file, new OkHttpUtils.OnCompleteListener<String>() {
+            @Override
+            public void onSuccess(String s) {
+                L.e("s="+s);
+                Result result = ResultUtils.getResultFromJson(s,User.class);
+                L.e("result="+result);
+                if(result==null){
+                    Toast.makeText(mContext, "更新用户头像失败", Toast.LENGTH_SHORT).show();
+                }else{
+                    User u = (User) result.getRetData();
+                    if(result.isRetMsg()){
+                        FuLiCenterApplication.setUser(u);
+                        ImageLoader.setAvatar(ImageLoader.getAvatarUrl(u),mContext,ivUserAvatar);
+                        Toast.makeText(mContext, "更新用户头像成功", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(mContext, "更新用户头像失败", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+                Toast.makeText(mContext, "更新用户头像失败", Toast.LENGTH_SHORT).show();
+                L.e("error="+error);
+            }
+        });
+
     }
 
     @OnClick({R.id.rl_avatar, R.id.rl_userName, R.id.rl_userNick, R.id.bt_signOut})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.rl_avatar:
+                mOnSetAvatarListener = new OnSetAvatarListener(mContext,R.id.layout_upload_avatar,
+                        user.getMuserName(),I.AVATAR_TYPE_USER_PATH);
                 break;
             case R.id.rl_userName:
                 Toast.makeText(mContext, "用户名作为登录名不能被修改", Toast.LENGTH_SHORT).show();;
@@ -95,5 +149,13 @@ public class PersonalSettingActivity extends BaseActivity {
             MFGT.gotoLogin(mContext);
         }
         finish();
+    }
+    private void showInfo(){
+        user = FuLiCenterApplication.getUser();
+        if(user!=null){
+            ImageLoader.setAvatar(ImageLoader.getAvatarUrl(user),mContext,ivUserAvatar);
+            tvUserName.setText(user.getMuserName());
+            tvUserNick.setText(user.getMuserNick());
+        }
     }
 }
--- app/src/main/java/cn/ucai/fulicenter/net/NetDao.java
+++ app/src/main/java/cn/ucai/fulicenter/net/NetDao.java
@@ -2,6 +2,8 @@ package cn.ucai.fulicenter.net;
 
 import android.content.Context;
 
+import java.io.File;
+
 import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.bean.BoutiqueBean;
 import cn.ucai.fulicenter.bean.CategoryChildBean;
@@ -106,6 +108,16 @@ public class NetDao {
                 .addParam(I.User.USER_NAME,userName)
                 .addParam(I.User.NICK,nick)
                 .targetClass(String.class)
+                .execute(listener);
+    }
+    public static void updateAvatar(Context context, String username, File file, OkHttpUtils.OnCompleteListener<String> listener){
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_UPDATE_AVATAR)
+                .addParam(I.NAME_OR_HXID,username)
+                .addParam(I.AVATAR_TYPE,I.AVATAR_TYPE_USER_PATH)
+                .addFile2(file)
+                .targetClass(String.class)
+                .post()
                 .execute(listener);
     }
 }
